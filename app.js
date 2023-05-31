00955e117c72c41417acc92648b29bd23a1110c7
zainabanani <mumfavouritebaby@gmail.com>
2023-05-31 15:46:06 +0100

    continuation of js

--- app.js
+++ app.js
@@ -143,51 +143,159 @@ console.log(fav);
 
 
 
-
-
 //numbers - 6.3 45000
-const year = 2003
-//boolean - true and false
+// math operators - + * / **(rasise to d power) %(returns the remainder of a division)
+// order of operations bedmas
+let balance = 500;
+const t1 = 20;
+const t2 = 100;
+const d1 = 200;
+
+
+// console.log(5 + 6 * 2 - 3); //5 + 12 - 3 // 17 - 3 =14
+
+// let total = 50;
+// //increase the value  of total by 10
+//   //shortcut kuma
+//  total += 10;
+// console.log(total);
+
+
+// let likes = 0
+// likes += 1;
+// likes += 1;
+// likes += 1;
+// likes -= 1;
+// likes ++;
+// likes --;
+// console.log(likes);
+
+
+// let post = 40
+// post += 15
+// post -= 15
+// console.log(post);
+
+
+//NaN Not a number
+console.log('4 - hello');
+console.log(3 ** 'hello');
+console.log(6 + 'zee');
+
+// Boolean - true and false 
+//used to execute commad
+
+
+
+const country =  'Nigeria';
+const continent = 'Africa';
+let population = 100000;
+console.log(country, continent, population);
+
+const isIsland = 'false';
+const language = 'English';
+
+
+// population -=50000;
+// console.log(population);
+
+population /=2;
+console.log(population);
+
+population ++
+console.log(population);
+
+//template literals
+//Nigeria is in african and its population of 67655687 people speak in french
+
+const description1 = `${country} is in ${continent} and its population of ${population} speaks in ${language}`
+console.log(description1);
+
+
+const description3 = `${country} as a country speaks ${language}`
+console.log(description3);
+
+
+//boolean - true or false
+//comparism operators- <> <= == ===
+// = assignment thats assigning values to variable
+// == loose equality doest care about data type
+// === strict equality is strict about data type
+console.log(6 < 3);
+console.log(5 >= 5.0);
+console.log(4 == 4);
+
+
+//Logical operators. we have 3 (and&&, or||, not!)
+console.log(5 > 6 && 2 == 2);
+console.log(6 > 7 || 2 === 2);
+console.log(5 != 5); 
+
+// type of function (typeof) data types
+
+//type conversion and type coercion
+//strings to number
+const str = "90";
+console.log(typeof str);
+const converted = Number(str);
+console.log(typeof converted);
+
+//numbers to string
+const num = 6875767
+console.log(typeof num);
+const converted2 = String(num)
+console.log(typeof converted2);
+
+//type of corecion
+const num1 = 8;
+const num2 = "2"
+
+console.log(num1 + num2);
+
+const res = num1 - num2
+console.log(res);
+ 
+
 //null - null
 //undefined -undefined (no value)
 
 
-const boyFriend = "Adam"
-const girlFriend = "Zeebaby"
+// const boyFriend = "Adam"
+// const girlFriend = "Zeebaby"
 
-console.log(boyFriend);
-console.log(girlFriend);
+// console.log(boyFriend);
+// console.log(girlFriend);
 
-const bayani = `my boyfriend name is ${boyFriend}`
-console.log(bayani);
+// const bayani = `my boyfriend name is ${boyFriend}`
+// console.log(bayani);
 
-console.log(boyFriend.toUpperCase ());
-console.log(girlFriend.toLowerCase ());
+// console.log(boyFriend.toUpperCase ());
+// console.log(girlFriend.toLowerCase ());
 
-console.log(boyFriend.includes('m'));
-console.log(boyFriend.includes('s'));
+// console.log(boyFriend.includes('m'));
+// console.log(boyFriend.includes('s'));
 
-console.log(boyFriend.startsWith('A'));
-console.log(girlFriend.endsWith('y'));
+// console.log(boyFriend.startsWith('A'));
+// console.log(girlFriend.endsWith('y'));
 
-console.log(boyFriend.indexOf('m'));
-console.log(girlFriend.indexOf('y'));
+// console.log(boyFriend.indexOf('m'));
+// console.log(girlFriend.indexOf('y'));
 
-console.log(girlFriend.slice(3, 5));
+// console.log(girlFriend.slice(3, 5));
 
-console.log(girlFriend.replaceAll('e', 'u'));
+// console.log(girlFriend.replaceAll('e', 'u'));
 
 
-const username2 = '     Hadiza'
-const password2 = '      23454r6454   '
-console.log(username2);
-console.log(password2);
+// const username2 = '     Hadiza'
+// const password2 = '      23454r6454   '
+// console.log(username2);
+// console.log(password2);
 
-console.log(username2.length);
-console.log(password2.length);
+// console.log(username2.length);
+// console.log(password2.length);
 
-console.log(username2.trim());
-console.log(password2.trimEnd());
-console.log(password2.trim());
+// console.log(username2.trim());
+// console.log(password2.trimEnd());
+// console.log(password2.trim());
 
-console.log(password2.length);
+// console.log(password2.length);
